6b71c3feb49b69e50ff733d790368b8106bfb0bb
Tim <u0416033@gmail.com>
2024-03-27 01:19:43 +0800

    0327

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import "./App.css";
 import Todo from "./components/todo";
+import Header from "./components/header";
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Todo></Todo>
     </div>
   );
